2e786296c93d4cab1a72552bc4a90156f21c7a92
Amq <3255440383@qq.com>
2026-04-02 18:24:56 +0800

    refactor(zhikestore): 重构智课仓库页面组件结构
    - 引入 ZhikestoreIndex 组件
    - 将页面内容包装在 div 容器中
    - 添加 ZhikestoreIndex 组件到模板中
    - 保持原有的页面标题显示

--- frontend/src/views/Zhikestore.vue
+++ frontend/src/views/Zhikestore.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+import ZhikestoreIndex from "@/components/zhikestore/zhikestoreIndex.vue";
 </script>
 
 <template>
-  <h1>这是智课仓库页面</h1>
+  <div>
+    <h1>这是智课仓库页面</h1>
+    <ZhikestoreIndex />
+  </div>
 </template>
 
 <style scoped>
